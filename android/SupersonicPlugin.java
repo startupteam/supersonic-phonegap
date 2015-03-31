79aa7e42201fb545b7979ca1d9d731be78d477d6
fu <indamail@mail.ru>
2015-03-31 17:35:13 +0500

    OfferWall close action.

--- android/SupersonicPlugin.java
+++ android/SupersonicPlugin.java
@@ -24,220 +24,225 @@ OnRewardedVideoListener,
 OnInterstitialListener,
 OnOfferWallListener {
 
-    private static final String LOGTAG = "SupersonicPlugin";
-    private static final String DEFAULT_APP_KEY = "32c9d67d";
+	private static final String LOGTAG = "SupersonicPlugin";
+	private static final String DEFAULT_APP_KEY = "32c9d67d";
 
-    private static final String ACTION_INITIALIZE = "initialize";
-    private static final String ACTION_SHOW_OFFERWALL = "showOfferwall";
-    private static final String ACTION_SHOW_REWARDEDVIDEO = "showRewardedVideo";
-    private static final String OPT_APPLICATION_KEY = "appKey";
-    private static final String OPT_USER_ID = "userId";
+	private static final String ACTION_INITIALIZE = "initialize";
+	private static final String ACTION_SHOW_OFFERWALL = "showOfferwall";
+	private static final String ACTION_CLOSE_OFFERWALL = "closeOfferwall";
+	private static final String ACTION_SHOW_REWARDEDVIDEO = "showRewardedVideo";
+	private static final String OPT_APPLICATION_KEY = "appKey";
+	private static final String OPT_USER_ID = "userId";
 
-    private String appKey = DEFAULT_APP_KEY;
-    private String userId = "5043b715c3bd823b760000ff";
-    private SSAPublisher ssaPub; 
+	private String appKey = DEFAULT_APP_KEY;
+	private String userId = "5043b715c3bd823b760000ff";
+	private SSAPublisher ssaPub;
+	private CallbackContext closeOWCallback;
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        PluginResult result = null;
-        
-        if (ACTION_INITIALIZE.equals(action)) {
-            JSONObject options = args.optJSONObject(0);
-            result = executeInitialize(options, callbackContext);
-        } else if (ACTION_SHOW_OFFERWALL.equals(action)) {
-            JSONObject options = args.optJSONObject(0);
-            result = executeShowOfferwall(options, callbackContext);
-        } else if (ACTION_SHOW_REWARDEDVIDEO.equals(action)) {
-            JSONObject options = args.optJSONObject(0);
-            result = executeShowRewardedVideo(options, callbackContext);
-        }
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		PluginResult result = null;
 
-        if (result != null) callbackContext.sendPluginResult( result );
+		if (ACTION_INITIALIZE.equals(action)) {
+			JSONObject options = args.optJSONObject(0);
+			result = executeInitialize(options, callbackContext);
+		} else if (ACTION_SHOW_OFFERWALL.equals(action)) {
+			JSONObject options = args.optJSONObject(0);
+			result = executeShowOfferwall(options, callbackContext);
+		} else if (ACTION_CLOSE_OFFERWALL.equals(action)) {
+			closeOWCallback = callbackContext;
+		} else if (ACTION_SHOW_REWARDEDVIDEO.equals(action)) {
+			JSONObject options = args.optJSONObject(0);
+			result = executeShowRewardedVideo(options, callbackContext);
+		}
 
-        return true;
-    }
+		if (result != null) callbackContext.sendPluginResult( result );
+
+		return true;
+	}
 /*
-    @Override
-    protected void onResume() {
-        super.onResume(false); 
-        if (ssaPub != null) {
-            ssaPub.onResume(this);
-        }
-    }
+	@Override
+	protected void onResume() {
+		super.onResume(false);
+		if (ssaPub != null) {
+			ssaPub.onResume(this);
+		}
+	}
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (ssaPub != null) {
-            ssaPub.onPause(this);
-        }
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (ssaPub != null) {
+			ssaPub.onPause(this);
+		}
+	}
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
 
-        if (ssaPub != null) {
-            ssaPub.release(this);
-        }
-    }*/
+		if (ssaPub != null) {
+			ssaPub.release(this);
+		}
+	}*/
 
-    private PluginResult executeInitialize(JSONObject options, CallbackContext callbackContext) {
-        Log.w(LOGTAG, "executeInitialize");
-        
-        this.initialize( options );
-        
-        callbackContext.success();
+	private PluginResult executeInitialize(JSONObject options, CallbackContext callbackContext) {
+		Log.w(LOGTAG, "executeInitialize");
 
-        return null;
-    }
+		this.initialize( options );
 
-    private void initialize( JSONObject options ) {
-        //if(options == null) return;
-        
-        if(options.has(OPT_APPLICATION_KEY)) this.appKey = options.optString( OPT_APPLICATION_KEY );
-        if(options.has(OPT_USER_ID)) this.userId = options.optString( OPT_USER_ID );
+		callbackContext.success();
 
-        ssaPub = SSAFactory.getPublisherInstance(this.webView.getContext());
-        
-        Map<String, String> params = null; 
-        ssaPub.initRewardedVideo(appKey, userId, params, this);
-    }
+		return null;
+	}
 
-    private PluginResult executeShowOfferwall(JSONObject options, CallbackContext callbackContext) {
-        Log.w(LOGTAG, "executeShowOfferwall");
-        
-        this.showOfferWall( options );
-        
-        callbackContext.success();
+	private void initialize( JSONObject options ) {
+		//if(options == null) return;
 
-        return null;
-    }
+		if(options.has(OPT_APPLICATION_KEY)) this.appKey = options.optString( OPT_APPLICATION_KEY );
+		if(options.has(OPT_USER_ID)) this.userId = options.optString( OPT_USER_ID );
 
-    private void showOfferWall(JSONObject options) {
-        Map<String, String> extraParams = new HashMap<String, String>();
-        ssaPub.showOfferWall(this.appKey, this.userId, extraParams, this);
-    }
-    
-    private PluginResult executeShowRewardedVideo(JSONObject options, CallbackContext callbackContext) {
-        Log.w(LOGTAG, "executeShowRewardedVideo");
-        
-        this.showRewardedVideo( );
-        
-        callbackContext.success();
+		ssaPub = SSAFactory.getPublisherInstance(this.webView.getContext());
 
-        return null;
-    }
+		Map<String, String> params = null;
+		ssaPub.initRewardedVideo(appKey, userId, params, this);
+	}
 
-    private void showRewardedVideo() {
-        ssaPub.showRewardedVideo();
-    }
+	private PluginResult executeShowOfferwall(JSONObject options, CallbackContext callbackContext) {
+		Log.w(LOGTAG, "executeShowOfferwall");
 
-    @Override
-    public void onGetOWCreditsFailed(String arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+		this.showOfferWall( options );
 
-    @Override
-    public void onOWAdClosed() {
-        // TODO Auto-generated method stub
-        
-    }
+		callbackContext.success();
 
-    @Override
-    public boolean onOWAdCredited(int arg0, int arg1, boolean arg2) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+		return null;
+	}
 
-    @Override
-    public void onOWGeneric(String arg0, String arg1) {
-        // TODO Auto-generated method stub
-        
-    }
+	private void showOfferWall(JSONObject options) {
+		Map<String, String> extraParams = new HashMap<String, String>();
+		ssaPub.showOfferWall(this.appKey, this.userId, extraParams, this);
+	}
 
-    @Override
-    public void onOWShowFail(String arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+	private PluginResult executeShowRewardedVideo(JSONObject options, CallbackContext callbackContext) {
+		Log.w(LOGTAG, "executeShowRewardedVideo");
 
-    @Override
-    public void onOWShowSuccess() {
-        // TODO Auto-generated method stub
-        
-    }
+		this.showRewardedVideo( );
 
-    @Override
-    public void onISAdClosed() {
-        // TODO Auto-generated method stub
-        
-    }
+		callbackContext.success();
 
-    @Override
-    public void onISGeneric(String arg0, String arg1) {
-        // TODO Auto-generated method stub
-        
-    }
+		return null;
+	}
 
-    @Override
-    public void onISInitFail(String arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+	private void showRewardedVideo() {
+		ssaPub.showRewardedVideo();
+	}
 
-    @Override
-    public void onISInitSuccess() {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void onOWAdClosed() {
+		Log.w(LOGTAG, "onOWAdClosed");
 
-    @Override
-    public void onISLoaded() {
-        // TODO Auto-generated method stub
-        
-    }
+		closeOWCallback.success();
+	}
 
-    @Override
-    public void onISLoadedFail(String arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void onGetOWCreditsFailed(String arg0) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void onRVAdClosed() {
-        // TODO Auto-generated method stub
-        
-    }
+	}
 
-    @Override
-    public void onRVAdCredited(int arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public boolean onOWAdCredited(int arg0, int arg1, boolean arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public void onRVGeneric(String arg0, String arg1) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void onOWGeneric(String arg0, String arg1) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void onRVInitFail(String arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+	}
 
-    @Override
-    public void onRVInitSuccess(AdUnitsReady arg0) {
-        // TODO Auto-generated method stub
-        Log.w(LOGTAG, "onRVInitSuccess");
-    }
+	@Override
+	public void onOWShowFail(String arg0) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void onRVNoMoreOffers() {
-        // TODO Auto-generated method stub
-        Log.w(LOGTAG, "onRVNoMoreOffers");
-    }
+	}
+
+	@Override
+	public void onOWShowSuccess() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onISAdClosed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onISGeneric(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onISInitFail(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onISInitSuccess() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onISLoaded() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onISLoadedFail(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRVAdClosed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRVAdCredited(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRVGeneric(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRVInitFail(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRVInitSuccess(AdUnitsReady arg0) {
+		// TODO Auto-generated method stub
+		Log.w(LOGTAG, "onRVInitSuccess");
+	}
+
+	@Override
+	public void onRVNoMoreOffers() {
+		// TODO Auto-generated method stub
+		Log.w(LOGTAG, "onRVNoMoreOffers");
+	}
 }
--- www/supersonic.js
+++ www/supersonic.js
@@ -1,4 +1,4 @@
-function SupersonicPlugin() {}
+SupersonicPlugin = function () {}
 
 SupersonicPlugin.prototype.initialize = function (appKey, userId, success, error) {
 	cordova.exec(success, error, 'Supersonic', 'initialize', [appKey, userId]);
@@ -6,6 +6,10 @@ SupersonicPlugin.prototype.initialize = function (appKey, userId, success, error
 
 SupersonicPlugin.prototype.showOfferwall = function (success, error) {
 	cordova.exec(success, error, 'Supersonic', 'showOfferwall', []);
+};
+
+SupersonicPlugin.prototype.closeOfferwall = function (success, error) {
+	cordova.exec(success, error, 'Supersonic', 'closeOfferwall', []);
 };
 
 SupersonicPlugin.prototype.showRewardedVideo = function (success, error) {
