da0306772f1a7454b8e08e5d4a56ea53c7ac8fe6
ravshan <khaydarov92@gmail.com>
2017-05-18 14:31:38 +0500

    integrate for new sdk iron source

--- android/SupersonicPlugin.java
+++ android/SupersonicPlugin.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 import android.util.Log;
 
-import com.supersonic.mediationsdk.logger.SupersonicError;
-import com.supersonic.mediationsdk.sdk.OfferwallListener;
-import com.supersonic.mediationsdk.sdk.Supersonic;
-import com.supersonic.mediationsdk.sdk.SupersonicFactory;
+import com.ironsource.mediationsdk.IronSource;
+import com.ironsource.mediationsdk.logger.IronSourceError;
+import com.ironsource.mediationsdk.sdk.OfferwallListener;
+
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -18,71 +18,57 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class SupersonicPlugin extends CordovaPlugin
-{
+public class SupersonicPlugin extends CordovaPlugin {
 
-	private static final String LOGTAG = "SupersonicPlugin";
-	private static final String DEFAULT_APP_KEY = "32c9d67d";
+    private static final String LOGTAG = "SupersonicPlugin";
+    private static final String DEFAULT_APP_KEY = "32c9d67d";
 
-	private static final String ACTION_INITIALIZE = "initialize";
-	private static final String ACTION_SHOW_OFFERWALL = "showOfferwall";
-	private static final String ACTION_CLOSE_OFFERWALL = "closeOfferwall";
-	private static final String OPT_APPLICATION_KEY = "appKey";
-	private static final String OPT_USER_ID = "userId";
+    private static final String ACTION_INITIALIZE = "initialize";
+    private static final String ACTION_SHOW_OFFERWALL = "showOfferwall";
+    private static final String ACTION_CLOSE_OFFERWALL = "closeOfferwall";
+    private static final String OPT_APPLICATION_KEY = "appKey";
+    private static final String OPT_USER_ID = "userId";
 
-	private String appKey = DEFAULT_APP_KEY;
-	private String userId = "5043b715c3bd823b760000ff";
-	private Supersonic mMediationAgent;
-	private CallbackContext closeOfferwallCallback;
+    private String appKey = DEFAULT_APP_KEY;
+    private String userId = "5043b715c3bd823b760000ff";
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		PluginResult result = null;
+    private CallbackContext closeOfferwallCallback;
 
-		if (ACTION_INITIALIZE.equals(action)) {
-			JSONObject options = args.optJSONObject(0);
-			result = executeInitialize(options, callbackContext);
-		} else if (ACTION_SHOW_OFFERWALL.equals(action)) {
-			JSONObject options = args.optJSONObject(0);
-			result = executeShowOfferwall(options, callbackContext);
-		} else if (ACTION_CLOSE_OFFERWALL.equals(action)) {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        PluginResult result = null;
+
+        if (ACTION_INITIALIZE.equals(action)) {
+            JSONObject options = args.optJSONObject(0);
+            result = executeInitialize(options, callbackContext);
+        } else if (ACTION_SHOW_OFFERWALL.equals(action)) {
+            JSONObject options = args.optJSONObject(0);
+            result = executeShowOfferwall(options, callbackContext);
+        } else if (ACTION_CLOSE_OFFERWALL.equals(action)) {
             closeOfferwallCallback = callbackContext;
-		}
+        }
 
-		if (result != null) callbackContext.sendPluginResult( result );
+        if (result != null) callbackContext.sendPluginResult(result);
 
-		return true;
-	}
-/*
-	@Override
-	protected void onResume() {
-		super.onResume(false);
-		if (ssaPub != null) {
-			ssaPub.onResume(this);
-		}
-	}
+        return true;
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		if (ssaPub != null) {
-			ssaPub.onPause(this);
-		}
-	}
+    @Override
+    public void onResume(boolean multitasking) {
+        super.onResume(multitasking);
+        IronSource.onResume(this.cordova.getActivity());
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
+    @Override
+    public void onPause(boolean multitasking) {
+        super.onPause(multitasking);
+        IronSource.onPause(this.cordova.getActivity());
+    }
 
-		if (ssaPub != null) {
-			ssaPub.release(this);
-		}
-	}*/
+    private PluginResult executeInitialize(final JSONObject options, final CallbackContext callbackContext) {
+        Log.w(LOGTAG, "executeInitialize");
 
-	private PluginResult executeInitialize(final JSONObject options, final CallbackContext callbackContext) {
-		Log.w(LOGTAG, "executeInitialize");
-
-		cordova.getThreadPool().execute(new Runnable() {
+        cordova.getThreadPool().execute(new Runnable() {
             public void run() {
                 initialize(options);
 
@@ -90,64 +76,73 @@ public class SupersonicPlugin extends CordovaPlugin
             }
         });
 
-		return null;
-	}
+        return null;
+    }
 
-	private void initialize(JSONObject options) {
+    private void initialize(JSONObject options) {
 
-		if (options.has(OPT_APPLICATION_KEY)) {
-			this.appKey = options.optString(OPT_APPLICATION_KEY);
-		}
-
-		if(options.has(OPT_USER_ID)) {
-			this.userId = options.optString(OPT_USER_ID);
-		}
-
-		OfferwallListener mOfferwallListener = new OfferwallListener()
-		{
-			public void onOfferwallInitSuccess() {}
-
-			public void onOfferwallInitFail(SupersonicError error) {}
-
-			public void onOfferwallOpened() {}
-
-			public void onOfferwallShowFail(SupersonicError supersonicError) {}
-
-			public boolean onOfferwallAdCredited(int credits, int totalCredits, boolean totalCreditsFlag) {
-				return false;
-			}
-
-			public void onGetOfferwallCreditsFail(SupersonicError supersonicError) {}
-
-			public void onOfferwallClosed() {
-				Log.w(LOGTAG, "onOfferwallClosed");
-
-				closeOfferwallCallback.success();
-			}
-		};
-
-		mMediationAgent = SupersonicFactory.getInstance();
-
-		mMediationAgent.setOfferwallListener(mOfferwallListener);
-
-		mMediationAgent.initOfferwall(this.cordova.getActivity(), this.appKey, this.userId);
-	}
-
-	private PluginResult executeShowOfferwall(JSONObject options, CallbackContext callbackContext) {
-		Log.w(LOGTAG, "executeShowOfferwall");
-
-		showOfferWall(options);
-
-		callbackContext.success();
-
-		return null;
-	}
-
-	private void showOfferWall(JSONObject options) {
-		Log.w(LOGTAG, "isOfferwallAvailable" + mMediationAgent.isOfferwallAvailable());
-
-        if (mMediationAgent.isOfferwallAvailable()) {
-            mMediationAgent.showOfferwall();
+        if (options.has(OPT_APPLICATION_KEY)) {
+            this.appKey = options.optString(OPT_APPLICATION_KEY);
         }
-	}
+
+        if (options.has(OPT_USER_ID)) {
+            this.userId = options.optString(OPT_USER_ID);
+        }
+
+        IronSource.setOfferwallListener(this.getOfferwallListener());
+        IronSource.setUserId(this.userId);
+        IronSource.init(this.cordova.getActivity(), this.appKey);
+    }
+
+    private PluginResult executeShowOfferwall(JSONObject options, CallbackContext callbackContext) {
+        Log.w(LOGTAG, "executeShowOfferwall");
+
+        showOfferWall(options);
+
+        callbackContext.success();
+
+        return null;
+    }
+
+    private void showOfferWall(JSONObject options) {
+        Log.w(LOGTAG, "isOfferwallAvailable " + IronSource.isOfferwallAvailable());
+
+        if (IronSource.isOfferwallAvailable()) {
+            IronSource.showOfferwall();
+        }
+    }
+
+    public OfferwallListener getOfferwallListener() {
+        OfferwallListener mOfferwallListener = new OfferwallListener() {
+            @Override
+            public void onOfferwallAvailable(boolean isAvailable) {
+            }
+
+            @Override
+            public void onOfferwallOpened() {
+            }
+
+            @Override
+            public void onOfferwallShowFailed(IronSourceError error) {
+            }
+
+            @Override
+            public boolean onOfferwallAdCredited(int credits, int totalCredits, boolean totalCreditsFlag) {
+                return false;
+            }
+
+            @Override
+            public void onGetOfferwallCreditsFailed(IronSourceError error) {
+            }
+
+            @Override
+            public void onOfferwallClosed() {
+                Log.w(LOGTAG, "onOfferwallClosed");
+
+                closeOfferwallCallback.success();
+            }
+        };
+
+        return mOfferwallListener;
+    }
 }
